dc989b33abbb4da0ac264163309b291fedc6042e
bronisMateusz <mateusz.bronis99@gmail.com>
2023-04-24 18:59:00 +0200

    Update configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// For some reasons this import doesn't work from vite.config.js
+import '@/assets/scss/styles.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
