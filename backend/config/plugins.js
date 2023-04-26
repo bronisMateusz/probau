2c81dcc3750d6811569811e9d4592c6085378999
bronisMateusz <mateusz.bronis99@gmail.com>
2023-04-26 08:01:54 +0200

    Add additional_attributes to menus plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,6 +1,34 @@
 module.exports = {
   menus: {
     config: {
+      layouts: {
+        menuItem: {
+          // This is the menu item edit panel.
+          link: [
+            // This is the "link" tab in the menu item edit panel.
+            {
+              input: {
+                label: "Additional attributes",
+                name: "additional_attributes",
+                type: "select",
+                options: [
+                  {
+                    label: "Telefon",
+                    value: "tel:",
+                  },
+                  {
+                    label: "Mail",
+                    value: "mailto:",
+                  },
+                ],
+              },
+              grid: {
+                col: 6,
+              },
+            },
+          ],
+        },
+      },
       maxDepth: 2,
     },
   },
