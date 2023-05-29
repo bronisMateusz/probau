89c610e98479f5019a2f6809b9e625bd35eb6769
bronisMateusz <mateusz.bronis99@gmail.com>
2023-05-29 22:58:31 +0200

    Fix font imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@fonts': fileURLToPath(new URL('./public/fonts', import.meta.url))
     }
   },
   server: {
